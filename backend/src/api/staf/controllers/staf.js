aecf8eb11d70a20e6127e4dcff7a66779b95c649
CamiloCVC <camicastellanos0602@gmail.com>
2023-01-11 20:22:44 -0500

    seteo de cloudinary

--- backend/src/api/staf/controllers/staf.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * staf controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::staf.staf');
--- backend/src/api/staf/services/staf.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * staf service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::staf.staf');
